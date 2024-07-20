be1a7299769489f45c00378a9d195b8368e8c6de
Mateusz Bogusławski <mateusz.boguslawski@ibnet.pl>
2024-07-20 12:50:21 +0200

    Add intro, display images from images/ directory

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@ along with this program.  If not, see https://www.gnu.org/licenses/.
 #define BUFFER_SIZE 100
 
 #define DISPLAY_TIME 5000 // 5 seconds
+#define INTRO_TIME 5000 // Two seconds
 
 #define TP_CS 4
 #define TP_IRQ 3
@@ -37,23 +38,40 @@ XPT2046_Touchscreen *touch;
 Calibration *calibration;
 SDStorage *storage;
 
+// Load image marked as current in SDStorage object
+// Return time needed for display (ms)
+uint32_t loadImage();
+
 void setup() {
-	display = new ILI9486(ILI9486::R2L_U2D);
+	display = new ILI9486(ILI9486::R2L_U2D, 0, ILI9486_BLACK);
 	
 	digitalWrite(ILI9486_CS, 1);
 	digitalWrite(4, 1);
-	storage = new SDStorage(5, display->getWidth(), display->getHeight(), "/");
+	storage = new SDStorage(5, display->getWidth(), display->getHeight(), "/images");
 	
 	touch = new XPT2046_Touchscreen(TP_CS, TP_IRQ);
 	touch->begin();
 	
 	calibration = new Calibration(true, display, touch);
 	calibration->calibrate(X_BEGIN, X_END, Y_BEGIN, Y_END);
+
+	storage->toImage("intro.bmp");
+	loadImage();
+	display->changeDefaultBacklight(UINT8_MAX);
+	display->setDefaultBacklight();
+	delay(INTRO_TIME);
+
 }
 
 void loop() {
-	uint16_t buffer[BUFFER_SIZE];
 	storage->nextImage();
+	loadImage();
+	delay(DISPLAY_TIME);
+}
+
+uint32_t loadImage() {
+	uint32_t time = millis();
+	uint16_t buffer[BUFFER_SIZE];
 
 	display->openWindow(0, 0, display->getWidth(), display->getHeight());
 	for (uint32_t i = 0; i < display->getSize() / BUFFER_SIZE; i++) {
@@ -61,7 +79,5 @@ void loop() {
 		display->writeBuffer(buffer, BUFFER_SIZE);
 	}
 
-	display->drawString(10, 460, (uint8_t*)storage->getCurrentImage().name(), ILI9486::L, ILI9486_BLACK);
-
-	delay(DISPLAY_TIME);
+	return millis() - time;
 }
